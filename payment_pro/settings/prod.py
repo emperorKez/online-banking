f898cac0008c1c7f325159f6d19df063b71cf7e1
Johnson Okezie <emperorkez@gmail.com>
2024-11-18 02:48:06 +0100

    code update

--- payment_pro/settings/prod.py
+++ payment_pro/settings/prod.py
@@ -42,6 +42,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
 # and renames the files with unique names for each version to support long-term caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 WHITENOISE_MANIFEST_STRICT = False
